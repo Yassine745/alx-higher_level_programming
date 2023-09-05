2c12e0e9e0a7e7b04928008a9f62687e410f4505
Yassine745 <motorex745@gmail.com>
2023-09-05 22:58:42 +0000

    python if else loops functions

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 11-pow.py
+
+
+def pow(a, b):
+    """Return a to the power of b."""
+    return (a ** b)#!/usr/bin/python3
+# 11-pow.py
+
+
+def pow(a, b):
+    """Return a to the power of b."""
+    return (a ** b)
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+Project of 0x01-python-if_else_loops_functions
